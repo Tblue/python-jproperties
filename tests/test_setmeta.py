7b15c6824a1ec0d8e995fdca55335c8154a8a00a
Tilman Blumenbach <tilman+git@ax86.net>
2015-12-26 00:28:51 +0100

    Tests: Check setting metadata values (str, bytes, int).

---
+++ tests/test_setmeta.py
@@ -0,0 +1,34 @@
+# vim: fileencoding=utf-8
+
+from jproperties import Properties
+from StringIO import StringIO
+
+def test_setmeta_bytes():
+    p = Properties()
+    p["a key"] = "the value", {b"metakey": b"metaval"}
+
+    out = StringIO()
+    p.store(out, strip_meta=False, timestamp=False)
+
+    out.seek(0)
+    assert out.read() == b"#: metakey=metaval\na\\ key=the value\n"
+
+def test_setmeta_unicode():
+    p = Properties()
+    p["a key"] = "the value", {u"metakey": u"ünicode metävalue!"}
+
+    out = StringIO()
+    p.store(out, encoding="utf-8", strip_meta=False, timestamp=False)
+
+    out.seek(0)
+    assert out.read() == b"#: metakey=ünicode metävalue\\!\na\\ key=the value\n"
+
+def test_setmeta_int():
+    p = Properties()
+    p["a key"] = "the value", {"metakey": 42}
+
+    out = StringIO()
+    p.store(out, strip_meta=False, timestamp=False)
+
+    out.seek(0)
+    assert out.read() == b"#: metakey=42\na\\ key=the value\n"
