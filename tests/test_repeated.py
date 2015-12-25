519210fa6724b3111873ad3ff7d0b6d3cf9ff0bd
Tilman Blumenbach <tilman+git@ax86.net>
2015-12-26 00:08:19 +0100

    Tests: Check if repeated keys (and metadata keys) are handled correctly.

---
+++ tests/test_repeated.py
@@ -0,0 +1,25 @@
+from jproperties import Properties
+
+def test_repeated():
+    p = Properties()
+    p.load("key:value\nkey=the value\nkey = value1\nkey : value2\nkey value3\nkey\tvalue4")
+
+    assert p.properties == {"key": "value4"}
+
+def test_repeated_with_meta():
+    p = Properties()
+    p.load("""
+        key = value1
+
+        #: metakey = metaval1
+        #: metakey2 = metaval22
+        key = value2
+
+        # Expected: Metadata should ONLY contain the following
+        # 'metakey' key.
+        #: metakey = metaval2
+        key = value3
+    """)
+
+    assert p.properties == {"key": "value3"}
+    assert p["key"] == ("value3", {"metakey": "metaval2"})
